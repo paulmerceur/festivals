8da3fa38985717abb8bed959cf85d328bc025246
paul <paul.merceur83@gmail.com>
2023-02-14 16:51:18 +0100

    liste benevoles

--- src/router.js
+++ src/router.js
@@ -1,18 +1,37 @@
 import { createWebHistory, createRouter } from "vue-router";
 import ListeJeux from "@/views/ListeJeux.vue";
 import JeuView from "@/views/JeuView.vue";
+import ListeBenevoles from "@/views/ListeBenevoles.vue";
+import BenevoleView from "@/views/BenevoleView.vue";
+import ListeZones from "@/views/ListeZones.vue";
 
 const routes = [
-  { 
-    name: 'ListeJeux',
-    path: '/', 
-    component: ListeJeux,
-  },
-  {
-    name: 'JeuView',
-    path: '/jeu/:id',
-    component: JeuView,
-  },
+    { 
+        name: 'ListeJeux',
+        path: '/',
+        alias: '/jeux', 
+        component: ListeJeux,
+      },
+    {
+        name: 'JeuView',
+        path: '/jeu/:id',
+        component: JeuView,
+    },
+    {
+        name: 'ListeBenevoles',
+        path: '/benevoles',
+        component: ListeBenevoles,
+    },
+    {
+        name: 'BenevoleView',
+        path: '/benevole/:id',
+        component: BenevoleView,
+    },
+    {
+        name: 'ListeZones',
+        path: '/zones',
+        component: ListeZones,
+    },
 ]
 
 const router = createRouter({
---
+++ src/views/ListeBenevoles.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="page">
+        <PageHeader></PageHeader>
+        <div class="list">
+            <ListItem :item="listHeader" :type="'benevole'" :isHeader=true></ListItem>
+            <ListItem v-for="benevole in benevoles" :key="benevole.id" :item="benevole" :type="'benevole'" @click="goToBenevoleView(benevole.id)"></ListItem>
+        </div>
+    </div>
+</template>
+
+<script>
+import ListItem from '../components/ListItem.vue'
+import PageHeader from '@/components/PageHeader.vue';
+export default {
+    name: 'ListeBenevoles',
+    components: {
+        ListItem,
+        PageHeader
+    },
+    data() { return {
+        benevoles: [],
+        listHeader: {prenom: "Nom", nom: "Type", email: "Zone"}
+    }},
+    methods: {
+        getBenevoles: async function() {
+            await fetch(this.$root.base_url + "benevoles/").then(res => res.json()).then(data => {
+                this.benevoles = data;
+            });
+        },
+        goToBenevoleView(id) {
+            localStorage.setItem('currentBenevoleId', id);
+            this.$router.push('benevole/' + id);
+        }
+    },
+    mounted() {
+        this.getBenevoles();
+    }
+}
+</script>
+
+<style>
+.logo {
+    width: 100px;
+    height: 100px;
+    margin: 40px auto;
+}
+
+.logo img {
+    width: 100%;
+}
+
+.list {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+</style>
